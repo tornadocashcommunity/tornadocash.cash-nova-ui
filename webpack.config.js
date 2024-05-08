9fe1d86a1cc6d47e63eee8594b81226e1eb67176
Tornado Contrib <tornadocontrib@proton.me>
2024-05-08 21:33:43 +0000

    Attempt to use ts-loader

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,23 @@ export default [
   {
     mode: 'production',
     entry: './assets/events.worker.js',
+    module: {
+      rules: [
+        {
+          test: /\.(tsx|ts)?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.worker.json'
+            }
+          }
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
       path: path.resolve('static'),
       filename: 'events.worker.js',
@@ -13,6 +30,23 @@ export default [
   {
     mode: 'production',
     entry: './assets/nullifier.worker.js',
+    module: {
+      rules: [
+        {
+          test: /\.(tsx|ts)?$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'ts-loader',
+            options: {
+              configFile: 'tsconfig.worker.json'
+            }
+          }
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
     output: {
       path: path.resolve('static'),
       filename: 'nullifier.worker.js',
